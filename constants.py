2f2dae414174033c7fae82070c254c3230949a8f
Sophia Cheto <sophiacheto@gmail.com>
2024-02-24 14:16:14 +0000

    Changes os constants

--- constants.py
+++ constants.py
@@ -5,13 +5,17 @@ EDGES_COLOR = (170, 170, 170) #Grey
 PLAYER_COLOR = (255,0,0) #Red
 IA_COLOR = (255,255,0) #Yellow
 
+# Constants for the data matrix
 ROWS = 6
 COLUMNS = 7
 
-SQUARESIZE = 100
-RADIUS = 45
-WIDTH = COLUMNS * SQUARESIZE
-HEIGHT = ROWS+1 * SQUARESIZE
+# Constants for the board image
+SQUARESIZE = 100    # size of each square that will divide the screen 
+RADIUS = 45     # radius of each player piece
+
+# Constants for the interface (scren)
+WIDTH = (4+COLUMNS) * SQUARESIZE    # width of the screen = board + 2 empty columns on each side of the screen
+HEIGHT = (ROWS+2) * SQUARESIZE    # height of the screen = board + 1 empty row under and above the board
    
         
        
