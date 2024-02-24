7653b1544a5ae4619b51e7c4508bac733bb39e96
Sophia Cheto <sophiacheto@gmail.com>
2024-02-24 14:50:15 +0000

    Changes on interface

--- board.py
+++ board.py
@@ -22,15 +22,22 @@ class Board:
         
     def draw_board(self):
         rad = c.RADIUS
+
+        self.screen.fill(c.BACKGORUND_COLOR)    # turns the background "background color"
+
+        shadow_coordinates = (2*self.pixels-10, self.pixels-10, self.columns*self.pixels+24, self.rows*self.pixels+24)
+        board_coordinates = (2*self.pixels-10, self.pixels-10, self.columns*self.pixels+20, self.rows*self.pixels+20)
+        pygame.draw.rect(self.screen, c.SHADOW_COLOR, shadow_coordinates, 0, 30)  # # draws the shadow with rounded corners
+        pygame.draw.rect(self.screen, c.BOARD_COLOR, board_coordinates, 0, 30)  # draws the board with rounded corners
+
+        # draw the board empty spaces:
         for col in range(self.columns):
             for row in range(self.rows):
-                rect_coordinates = (col*self.pixels, row*self.pixels+self.pixels, self.pixels, self.pixels)
                 # center_of_circle = (col * self.pixels + self.pixels // 2, row * self.pixels + self.pixels // 2)
-
-                center_of_circle = (int(col*self.pixels+self.pixels/2), int(row*self.pixels+self.pixels+self.pixels/2))
-                pygame.draw.rect(self.screen, c.TABLE_COLOR, rect_coordinates)
+                center_of_circle = (int((col+5/2)*self.pixels), int((row+3/2)*self.pixels))
                 pygame.draw.circle(self.screen, c.BACKGORUND_COLOR, center_of_circle, rad)
         
+
         for col in range(self.columns):
             for row in range(self.rows):	
                 center_of_circle = (int(col*self.pixels+self.pixels/2), self.height-int(row*self.pixels+self.pixels/2))
