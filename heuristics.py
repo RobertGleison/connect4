54dffb547d4e2b3b7fc87dbe275836196ec3e087
Sophia Cheto <sophiacheto@gmail.com>
2024-03-03 13:23:25 +0000

    fixed sliding windows

--- heuristics.py
+++ heuristics.py
@@ -11,9 +11,11 @@ def a_star(board: np.ndarray, ai_piece: int, opponent_piece: int) -> int:
     best_move = -1
     for col in range(c.COLUMNS):
         if not game.is_valid(board, col): continue
-        # temp_score = 0
-        row = game.get_next_open_row(board, col)
-        temp_score = calculate_score(board, row, col, ai_piece, opponent_piece)
+        temp_score = 0
+        board_copy = board.copy()
+        row = game.get_next_open_row(board_copy, col)
+        game.drop_piece(board_copy, row, col, 2)
+        temp_score = calculate_score(board_copy, row, col, ai_piece, opponent_piece)
         if temp_score > move_score:
             best_move = col
             move_score = temp_score
@@ -45,7 +47,7 @@ def calculate_score(board: np.ndarray, row: int, column: int, ai_piece: int, opp
     logging.info(f"3 ocorrencia ai: {score_ai3}")
     # Linha com 4
     score_ai4 += 1000 * get_ocurrences(board, 4, ai_piece, row, column)
-    logging.info(f"3 ocorrencia ai: {score_ai3}")
+    logging.info(f"4 ocorrencia ai: {score_ai4}")
     #Linha com 1 (Inimigo)
     score_opponent1 += 1 * get_ocurrences(board, 1, opponent_piece, row, column)
     logging.info(f"1 ocorrencia oponente: {score_opponent1}")
@@ -57,7 +59,7 @@ def calculate_score(board: np.ndarray, row: int, column: int, ai_piece: int, opp
     logging.info(f"3 ocorrencia oponente: {score_opponent3}")
     #Linha com 4 (Inimigo)
     score_opponent4 += 1000 * get_ocurrences(board, 4, opponent_piece, row, column)
-    logging.info(f"3 ocorrencia oponente: {score_opponent3}")
+    logging.info(f"4 ocorrencia oponente: {score_opponent4}")
 
     score_ai = score_ai1 + score_ai2 + score_ai3
     score_opponent = score_opponent1 + score_opponent2 + score_opponent3
@@ -72,8 +74,8 @@ def get_ocurrences(board: np.ndarray, reference_quantity: int, piece: int, row: 
     # Check horizontal
     for col in range(c.COLUMNS - 3):
         temp = 0
-        for row in range(c.ROWS):
-            sliding_window = [board[row][col + i] for i in range(4)]
+        for r in range(c.ROWS):
+            sliding_window = [board[r][col + i] for i in range(4)]
             for board_piece in sliding_window:
                 if board_piece == piece:
                     temp += 1
@@ -83,8 +85,8 @@ def get_ocurrences(board: np.ndarray, reference_quantity: int, piece: int, row: 
     # Check vertical
     for col in range(c.COLUMNS):
         temp = 0
-        for row in range(c.ROWS - 3):
-            sliding_window = [board[row + i][column] for i in range(4)]
+        for r in range(c.ROWS - 3):
+            sliding_window = [board[r + i][col] for i in range(4)]
             for board_piece in sliding_window:
                 if board_piece == piece:
                     temp += 1
@@ -94,8 +96,8 @@ def get_ocurrences(board: np.ndarray, reference_quantity: int, piece: int, row: 
     # Check ascending diagonal
     for col in range(c.COLUMNS - 3):
         temp = 0
-        for row in range(c.ROWS - 3):
-            sliding_window = [board[row + i][col + i] for i in range(4)]
+        for r in range(c.ROWS - 3):
+            sliding_window = [board[r + i][col + i] for i in range(4)]
             for board_piece in sliding_window:
                 if board_piece == piece:
                     temp += 1
@@ -115,21 +117,21 @@ def get_ocurrences(board: np.ndarray, reference_quantity: int, piece: int, row: 
     return occurrences
 
 # para implementar ainda
-def a_star_melhorado(board: np.ndarray, ai_piece: int, opponent_piece: int) -> int:
-    move_score = -10000
-    best_move = -1
-    for col in range(c.COLUMNS):
-        if not game.is_valid(board, col): continue
-        board_copy = board.copy()
-        temp_score = 0
-        row = game.get_next_open_row(board_copy, col)
-        # add peça no novo tabuleiro falso para cada coluna
-        # chamar a_star para o oponente e retornar aqui apenas com o tabuleiro que contenha a melhor peça jogada dele 
-        temp_score = calculate_score(board, row, col, ai_piece, opponent_piece)
-        if temp_score > move_score:
-            best_move = col
-            move_score = temp_score
-    return best_move
+# def a_star_melhorado(board: np.ndarray, ai_piece: int, opponent_piece: int) -> int:
+#     move_score = -10000
+#     best_move = -1
+#     for col in range(c.COLUMNS):
+#         if not game.is_valid(board, col): continue
+#         board_copy = board.copy()
+#         cur_score = 0
+#         row = game.get_next_open_row(board_copy, col)
+#         # add peça no novo tabuleiro falso para cada coluna
+#         # chamar a_star para o oponente e retornar aqui apenas com o tabuleiro que contenha a melhor peça jogada dele 
+#         cur_score = calculate_score(board, row, col, ai_piece, opponent_piece)
+#         if cur_score > move_score:
+#             best_move = col
+#             move_score = cur_score
+#     return best_move
 
 
 def alphabeta(board: np.ndarray, piece: int) -> int:
