977dbcc2537b101bc6a6fc66b3d2163832fe9758
robert <robertgleison12@gmail.com>
2024-03-08 15:07:25 +0000

    Fixed bug on choose column out of range

--- alpha_beta.py
+++ alpha_beta.py
@@ -7,18 +7,17 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 def alpha_beta(board):
-    logging.info("Enter alpha beta")
+    best_score = float('-inf')
     best_move = -1
     depth_limit = 3
-    current_value = float('-inf')
     children = get_childrens(board, c.AI_PIECE)
-
-    for i in (range(len(children))):
-        score = min_value(board, 0, float('-inf'), float('+inf'), depth_limit, c.HUMAN_PIECE)
-        logging.info(f"Score for root for column {i+1} = {score}")
-        if score > current_value:
-            current_value = score
-            best_move = i
+    for col in (range(c.COLUMNS)):
+        if not game.is_valid(board, col): continue
+        current_score = min_value(board, 0, float('-inf'), float('+inf'), depth_limit, c.HUMAN_PIECE)
+        logging.info(f"Score for root for column {col+1} = {current_score}")
+        if current_score > best_score:
+            best_move = col
+            best_score = current_score
     return best_move
 
 
@@ -31,9 +30,9 @@ def max_value(board, depth, alpha, beta, depth_limit: int, piece: int):
         return h.calculate_board_score(board, c.HUMAN_PIECE, c.AI_PIECE)
 
     for move in childrens:
-        eval = min_value(move, depth+1, alpha, beta, depth_limit, c.AI_PIECE)
-        score = max(eval, max_eval)
-        alpha = max(alpha, eval)
+        score = min_value(move, depth+1, alpha, beta, depth_limit, c.AI_PIECE)
+        score = max(score, max_eval)
+        alpha = max(alpha, score)
         if beta <= alpha: break
     logging.info(f"Score: {score}")
     return score
@@ -43,13 +42,12 @@ def min_value(board, depth: int, alpha: int, beta: int, depth_limit: int, piece:
     logging.info("Enter min_value")
     if game.winning_move(board, piece) or depth == depth_limit:
         return h.calculate_board_score(board, c.HUMAN_PIECE, c.AI_PIECE)
-    logging.info("Passou to winning_move validations o min value")
     min_eval = float('+inf')
     childrens = get_childrens(board, c.HUMAN_PIECE)
     for move in childrens:
-        eval = max_value(move, depth+1, alpha, beta, depth_limit, c.HUMAN_PIECE)
-        score = min(eval, min_eval)
-        beta = min(beta, eval)
+        score = max_value(move, depth+1, alpha, beta, depth_limit, c.HUMAN_PIECE)
+        score = min(score, min_eval)
+        beta = min(beta, score)
         if beta <= alpha: break
     logging.info(f"Score: {score}")
     return score
