b45586fd197618951001ff2187f4f0e23e4df821
robert <robertgleison12@gmail.com>
2024-03-08 14:42:06 +0000

    Fixed same score for each column bug

--- alpha_beta.py
+++ alpha_beta.py
@@ -2,17 +2,20 @@ import numpy as np
 import constants as c
 import heuristics as h
 import game_logic as game
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 def alpha_beta(board):
+    logging.info("Enter alpha beta")
     best_move = -1
-    depth = 5
+    depth_limit = 3
     current_value = float('-inf')
     children = get_childrens(board, c.AI_PIECE)
 
     for i in (range(len(children))):
-        if(h.calculate_board_score(children[i],c.HUMAN_PIECE, c.AI_PIECE) == 512):
-            return children[i]
-        score = max_value(board, depth, float('-inf'), float('+inf'), depth, c.HUMAN_PIECE)
+        score = min_value(board, 0, float('-inf'), float('+inf'), depth_limit, c.HUMAN_PIECE)
+        logging.info(f"Score for root for column {i+1} = {score}")
         if score > current_value:
             current_value = score
             best_move = i
@@ -20,30 +23,35 @@ def alpha_beta(board):
 
 
 def max_value(board, depth, alpha, beta, depth_limit: int, piece: int):
+    logging.info("Enter max_value")
+    max_eval = float('-inf')
+    childrens = get_childrens(board, c.AI_PIECE)
+
     if game.winning_move(board, piece) or depth == depth_limit:
         return h.calculate_board_score(board, c.HUMAN_PIECE, c.AI_PIECE)
 
-    max_eval = float('-inf')
-    childrens = get_childrens(board, c.AI_PIECE)
     for move in childrens:
-        eval = min_value(move, depth-1, alpha, beta, c.AI_PIECE)
+        eval = min_value(move, depth+1, alpha, beta, depth_limit, c.AI_PIECE)
         score = max(eval, max_eval)
         alpha = max(alpha, eval)
         if beta <= alpha: break
+    logging.info(f"Score: {score}")
     return score
 
 
 def min_value(board, depth: int, alpha: int, beta: int, depth_limit: int, piece: int):
+    logging.info("Enter min_value")
     if game.winning_move(board, piece) or depth == depth_limit:
         return h.calculate_board_score(board, c.HUMAN_PIECE, c.AI_PIECE)
-
+    logging.info("Passou to winning_move validations o min value")
     min_eval = float('+inf')
     childrens = get_childrens(board, c.HUMAN_PIECE)
     for move in childrens:
-        eval = max_value(move, depth-1, alpha, beta, c.HUMAN_PIECE)
+        eval = max_value(move, depth+1, alpha, beta, depth_limit, c.HUMAN_PIECE)
         score = min(eval, min_eval)
         beta = min(beta, eval)
         if beta <= alpha: break
+    logging.info(f"Score: {score}")
     return score
 
 
--- heuristics.py
+++ heuristics.py
@@ -9,30 +9,30 @@ def calculate_board_score(board: np.ndarray, piece: int, opponent_piece: int) ->
     for col in range(c.COLUMNS - 3):
         for r in range(c.ROWS):
             segment = [board[r][col + i] for i in range(4)]
-            score += count_cur(segment, piece, opponent_piece)
+            score += weights(segment, piece, opponent_piece)
 
     # Check vertical
     for col in range(c.COLUMNS):
         for r in range(c.ROWS - 3):
             segment = [board[r + i][col] for i in range(4)]
-            score += count_cur(segment, piece, opponent_piece)
+            score += weights(segment, piece, opponent_piece)
 
     # Check ascending diagonal
     for col in range(c.COLUMNS - 3):
         for r in range(c.ROWS - 3):
             segment = [board[r + i][col + i] for i in range(4)]
-            score += count_cur(segment, piece, opponent_piece)
+            score += weights(segment, piece, opponent_piece)
 
     # Check descending diagonal
     for col in range(c.COLUMNS - 3):
         for r in range(3, c.ROWS):
             segment = [board[r - i][col + i] for i in range(4)]
-            score += count_cur(segment, piece, opponent_piece)
+            score += weights(segment, piece, opponent_piece)
 
     return score
 
 
-def count_cur(segment: list, piece: int, opponent_piece: int) -> int:
+def weights(segment: list, piece: int, opponent_piece: int) -> int:
     if piece in segment and opponent_piece in segment: return 0
     if segment.count(piece) == 1: return 1
     if segment.count(piece) == 2: return 10
