820b9fb16f5ae8485c34984e40b65694ceb6f740
Sophia Cheto <sophiacheto@gmail.com>
2024-03-28 03:14:13 +0000

    fixed alpha beta

--- ai_algorithms/alpha_beta.py
+++ ai_algorithms/alpha_beta.py
@@ -1,76 +1,63 @@
-from game_rules import constants as c
+from game_rules import constants as c, game_logic as game
 from heuristics import heuristic as h
-from game_rules import game_logic as game
-import time
-import logging
+import time, logging, numpy as np
 
-#O tempo de resposta está compatível com o trabalho do matheus. No começo demora próximo de 1 segundo, mas depois passa a ser 0.5 segundos ou 0.3 segundos
+
 NODES_VISITED = 1
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
-def alpha_beta(board):
+
+def alpha_beta(board: np.ndarray, profundidade):
     start_time = time.time()
     global NODES_VISITED
     NODES_VISITED = 1
-    best_score = float('-inf')
-    best_move = -1
-    depth_limit = 5
     children = get_children(board, c.AI_PIECE)
-
-    for col in range(c.COLUMNS):
-        child_board = children[col]
-        if not game.is_valid(child_board, col): continue
-        current_score = min_value(child_board, 0, float('-inf'), float('+inf'), depth_limit, c.HUMAN_PIECE)
-        if current_score > best_score:
-            best_move = col
-            best_score = current_score
-        logging.info(f"Number of nodes visited = {NODES_VISITED}")
-        logging.info(f"Score = {best_score}")
+    melhor_jogada = -1
+    melhor_score = float('-inf')
+    for (child, col) in children:
+        score = calcular(child, 0, float('-inf'), float('+inf'), profundidade, False)
+        if score > melhor_score:
+            melhor_score = score
+            melhor_jogada = col
     end_time = time.time()
     logging.info(f"Tempo de resposta = {end_time-start_time}")
+    return melhor_jogada
+
+
+
+def calcular(board: np.ndarray, depth: int, alpha: int, beta: int, depth_limit: int, maximizing):
+    if depth == depth_limit or game.winning_move(board, 1) or game.winning_move(board, 2):
+        return h.calculate_board_score(board, c.AI_PIECE, c.HUMAN_PIECE)
+
     
-    return best_move
+    if maximizing:
+        maxEval = float('-inf')
+        children = get_children(board, c.AI_PIECE)
+        for (child, col) in children:
+            eval = calcular(child, depth+1, alpha, beta, depth_limit, False)
+            maxEval = max(maxEval, eval)
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        return maxEval
+    
+    else:
+        minEval = float('+inf')
+        children = get_children(board, c.HUMAN_PIECE)
+        for (child, col) in children:
+            eval = calcular(child, depth+1, alpha, beta, depth_limit, True)
+            minEval = min(minEval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return minEval
 
 
-def max_value(board, depth, alpha, beta, depth_limit: int, piece: int):
-    max_eval = float('-inf')
-    childrens = get_children(board, c.AI_PIECE)
 
-    if game.winning_move(board, piece) or depth == depth_limit:
-        # return te.evaluate_board(board)
-        return h.calculate_board_score(board, c.AI_PIECE, c.HUMAN_PIECE)
-
-    for move in childrens:
-        score = min_value(move, depth+1, alpha, beta, depth_limit, c.AI_PIECE)
-        global NODES_VISITED
-        NODES_VISITED+=1
-        score = max(score, max_eval)
-        alpha = max(alpha, score)
-        if beta <= alpha: break
-    # logging.info(f"Score: {score}")
-    return score
-
-
-def min_value(board, depth: int, alpha: int, beta: int, depth_limit: int, piece: int):
-    if game.winning_move(board, piece) or depth == depth_limit:
-        # return te.evaluate_board(board)
-        return h.calculate_board_score(board, c.AI_PIECE, c.HUMAN_PIECE)
-    min_eval = float('+inf')
-    childrens = get_children(board, c.HUMAN_PIECE)
-    for move in childrens:
-        score = max_value(move, depth+1, alpha, beta, depth_limit, c.HUMAN_PIECE)
-        global NODES_VISITED
-        NODES_VISITED+=1
-        score = min(score, min_eval)
-        beta = min(beta, score)
-        if beta <= alpha: break
-    # logging.info(f"Score: {score}")
-    return score
-
-
-def get_children(board, piece: int) -> list:
-    childrens = []
-    for col in range(c.COLUMNS):
-        copy_board = game.simulate_move(board, piece, col)
-        childrens.append(copy_board)
-    return childrens
+def get_children(board, piece) -> None:
+        children = []
+        if game.available_moves(board) == -1: return  
+        for col in game.available_moves(board):  
+            copy_board = game.simulate_move(board, piece, col)   
+            children.append((copy_board, col)) 
+        return children
